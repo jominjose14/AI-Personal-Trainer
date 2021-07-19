a23fa99a5076b810d094543430f3892fa10ffcda
jominjose14 <42197279+jominjose14@users.noreply.github.com>
2021-07-19 14:58:38 +0530

    Working app

---
+++ util/faceModule.py
@@ -0,0 +1,77 @@
+import cv2
+import mediapipe as mp
+import time
+
+class FaceDetector():
+    def __init__(self,minDetectionCon=0.5):
+        self.minDetectionCon=minDetectionCon
+        self.mpFaceDetection = mp.solutions.face_detection
+        self.mpDraw = mp.solutions.drawing_utils
+        self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon)  # pass in confidence
+
+    def findFaces(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceDetection.process(imgRGB)
+        bboxes = []
+
+        if self.results.detections:
+            for id, detection in enumerate(self.results.detections):
+                # mpDraw.draw_detection(img, detection)
+                # print(id, detection)
+                # print(detection.score)
+                # print(detection.location_data.relative_bounding_box)
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+                bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                    int(bboxC.width * iw), int(bboxC.height * ih)
+                bboxes.append([bbox,detection.score])
+                if draw:
+                    img = self.fancyDraw(img, bbox)
+                    cv2.putText(img, f'FPS: {int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+
+        return img, bboxes
+
+    def fancyDraw(self, img, bbox, l=30, lthick=5, rthink=1):
+        x, y, w, h = bbox
+        x1, y1 = x+w, y+h
+
+        cv2.rectangle(img, bbox, (255, 0, 255), rthink)
+
+        # top left
+        cv2.line(img, (x,y), (x+l, y), (255,0,255), lthick)
+        cv2.line(img, (x, y), (x, y+l), (255, 0, 255), lthick)
+
+        # top right
+        cv2.line(img, (x+w, y), (x +w- l, y), (255, 0, 255), lthick)
+        cv2.line(img, (x+w, y), (x+w, y + l), (255, 0, 255), lthick)
+
+        # bottom right
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), lthick)
+        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), lthick)
+
+        # bottom left
+        cv2.line(img, (x1-w, y1), (x1-w + l, y1), (255, 0, 255), lthick)
+        cv2.line(img, (x1-w, y1), (x1-w, y1 - l), (255, 0, 255), lthick)
+        return img
+
+def main():
+    # cap = cv2.VideoCapture('videos/kids.mp4')
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+    detector = FaceDetector()
+
+    while True:
+        success, img = cap.read()
+        img, bboxes = detector.findFaces(img)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    main()
